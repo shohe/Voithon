e205c75cf44845a4e3744964e877f0713d850f97
SHOHE <ohtani.s@nagisa-inc.jp>
2015-06-06 21:36:57 +0900

    GoalViewController+

---
+++ Voithon/UI/Goal/GoalViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GoalViewController.swift
+//  Voithon
+//
+//  Created by SHOHE on 6/6/15.
+//  Copyright (c) 2015 OhtaniShohe. All rights reserved.
+//
+
+import UIKit
+
+class GoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
